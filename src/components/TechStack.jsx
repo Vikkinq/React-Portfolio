cf42c4f8a13126ac5140df890ec65b0e43902f3b
Symon <fritzsymon23@gmail.com>
2026-03-21 23:41:09 +0800

    Added new Techology Stacks and Experience Data

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -14,7 +14,12 @@ export default function TechStack() {
         {stack.map((tech, index) => (
           <Reveal key={index}>
             <span className="inline-flex items-center gap-3 px-3 py-2 rounded-lg border border-white/10 bg-white/10 text-sm cursor-pointer hover:bg-white/20 transition-all duration-300">
-              <i className={tech.icon}></i> {tech.stack}
+              {tech.icon === "img" ? (
+                <img src={tech.iconUrl} alt={tech.stack} className="w-4 h-4" />
+              ) : (
+                <i className={tech.icon}></i>
+              )}
+              {tech.stack}
             </span>
           </Reveal>
         ))}
